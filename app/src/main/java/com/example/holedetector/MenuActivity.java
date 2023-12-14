870fe054fc881e716ce693a52bcecc580c9a7d50
piotr-grzegorzek <49654876+piotr-grzegorzek@users.noreply.github.com>
2023-12-14 10:59:15 +0100

    feat: add main menu

---
+++ app/src/main/java/com/example/holedetector/MenuActivity.java
@@ -0,0 +1,32 @@
+package com.example.holedetector;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Find buttons by their IDs
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+
+        // Set click listeners for the buttons
+        button1.setOnClickListener(v -> {
+            // Handle button1 click
+            // For example: launch a new activity, perform an action, etc.
+        });
+
+        button2.setOnClickListener(v -> {
+            // Handle button2 click
+            // For example: launch a new activity, perform an action, etc.
+        });
+
+        // Add more listeners or logic for other UI elements as needed
+    }
+}
