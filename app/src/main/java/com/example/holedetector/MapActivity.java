d5120839208cdab8a82aa5db4e2a1024f48ee7d0
piotr-grzegorzek <49654876+piotr-grzegorzek@users.noreply.github.com>
2023-12-20 11:38:55 +0100

    refactor: copilot

--- app/src/main/java/com/example/holedetector/MapActivity.java
+++ app/src/main/java/com/example/holedetector/MapActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
-import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -21,55 +23,43 @@ import org.osmdroid.views.overlay.Marker;
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteException;
-
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class MapActivity extends AppCompatActivity {
 
-    private final String DBNAME = "Bazunia.db"; //Nazwa dla nowej bazy
-    SQLiteDatabase baza = null;
-
-    private List<Marker> markers = new ArrayList<Marker>();
-
+    private final String DBNAME = "Bazunia.db"; // Name for the new database
+    private SQLiteDatabase baza = null;
+    private List<Marker> markers = new ArrayList<>();
 
     @SuppressLint("Range")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        Context ctx = getApplicationContext();
-        //important! set your user agent to prevent getting banned from the osm servers
-        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
-
         setContentView(R.layout.activity_map);
 
+        Context ctx = getApplicationContext();
+        // Important! Set your user agent to prevent getting banned from the OSM servers
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
 
-        MapView map = (MapView) findViewById(R.id.map);
+        MapView map = findViewById(R.id.map);
         map.setTileSource(TileSourceFactory.MAPNIK);
         map.setBuiltInZoomControls(true);
         map.setMultiTouchControls(true);
 
         IMapController mapController = map.getController();
-        mapController.setZoom(9);
+        mapController.setZoom(20);
         GeoPoint startPoint = new GeoPoint(53.2544, 14.3310);
         mapController.setCenter(startPoint);
 
         // Enable the location overlay
-map.getOverlays().add(new MyLocationNewOverlay(new GpsMyLocationProvider(this), map));
+        MyLocationNewOverlay myLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(this), map);
+        myLocationOverlay.enableMyLocation();
+        myLocationOverlay.enableFollowLocation();
+        map.getOverlays().add(myLocationOverlay);
 
-// Set the map controller to your current location
-MyLocationNewOverlay myLocationOverlay = (MyLocationNewOverlay) map.getOverlays().get(0);
-myLocationOverlay.enableMyLocation();
-myLocationOverlay.enableFollowLocation();
-
-
-        // add markers
-        try{
+        // Add markers
+        try {
             baza = this.openOrCreateDatabase(DBNAME, MODE_PRIVATE, null);
             baza.execSQL("CREATE TABLE IF NOT EXISTS Markers (x FLOAT, y FLOAT)");
 
@@ -77,18 +67,15 @@ myLocationOverlay.enableFollowLocation();
             dodaj(22.78f, 44.32f);
             dodaj(55.225f, 64.21f);
 
-            String sx = null, sy = null;
+            String sx, sy;
 
-            Cursor cursor = baza.rawQuery("SELECT * FROM Markers",null);
-            if(cursor.moveToFirst()) { //Metoda zwraca FALSE jesli cursor jest pusty
+            Cursor cursor = baza.rawQuery("SELECT * FROM Markers", null);
+            if (cursor.moveToFirst()) {
                 do {
-                    //getString Returns the value of the requested column as a String.
-                    //getColumnIndex Returns the zero-based index for the given column name,
-                    //	or -1 if the column doesn't exist.
-
                     sx = cursor.getString(cursor.getColumnIndex("x"));
                     sy = cursor.getString(cursor.getColumnIndex("y"));
-                    float x = Float.valueOf(sx), y = Float.valueOf(sy);
+                    float x = Float.parseFloat(sx);
+                    float y = Float.parseFloat(sy);
 
                     GeoPoint pkt = new GeoPoint(x, y);
                     Marker m = new Marker(map);
@@ -96,44 +83,33 @@ myLocationOverlay.enableFollowLocation();
                     m.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
                     markers.add(m);
 
-                } while (cursor.moveToNext()); //Metoda zwraca FALSE wówczas gdy cursor przejdzie ostatni wpis
+                } while (cursor.moveToNext());
                 cursor.close();
             }
 
             baza.close();
-        }
-        catch(SQLiteException e) {
-            Log.e(getClass().getSimpleName(), "Could not create or Open the database");
+        } catch (SQLiteException e) {
+            Log.e(getClass().getSimpleName(), "Could not create or open the database");
         }
 
         for (Marker m : markers) {
             map.getOverlays().add(m);
         }
 
-
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(view -> {
-            //Close this activity
-            finish();
-        });
+        fab.setOnClickListener(view -> finish());
     }
 
-    public void onResume(){
+    @Override
+    protected void onResume() {
         super.onResume();
-        //this will refresh the osmdroid configuration on resuming.
-        //if you make changes to the configuration, use
-        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        //Configuration.getInstance().save(this, prefs);
         Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
     }
 
-
-    private void dodaj(float x, float y){
-        //baza.execSQL("INSERT INTO Markers Values(53.2544, 14.3310);");
+    private void dodaj(float x, float y) {
         ContentValues vals = new ContentValues();
         vals.put("x", x);
         vals.put("y", y);
         baza.insert("Markers", null, vals);
     }
-
 }
